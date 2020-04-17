87ed151c5fb0d1ba41b74daefed82589f8df0b69
kskrajny <j.skrajny@student.uw.edu.pl>
2020-04-17 18:01:25 +0200

    juhu

--- err.h
+++ err.h
@@ -1,11 +1,8 @@
 #ifndef _ERR_
 #define _ERR_
 
-/* Wypisuje informację o błędnym zakończeniu funkcji systemowej
+/* Wypisuje komunikat o błędzie
 i kończy działanie programu. */
 extern void syserr(const char *fmt, ...);
-
-/* Wypisuje informację o błędzie i kończy działanie programu. */
-extern void fatal(const char *fmt, ...);
 
 #endif
